b6558df58fcc9eb20cf5ae7febbd263af449b95c
Satoshi Hattori <>
2023-01-02 23:43:25 +0900

    update

--- Sources/SwiftCloudSample/SwiftCloudSample.swift
+++ Sources/SwiftCloudSample/SwiftCloudSample.swift
@@ -4,25 +4,49 @@ import Compute
 @available(iOS 13.0.0, *)
 @main
 public struct SwiftCloudSample {
-    public private(set) static var text = "Hello, World!!"
+
+    static let router = Router()
+        .get("") { req, res in
+            try await res
+                .status(.ok)
+                .send("Hello, World!")
+        }
+        .get("/check") { req, res in
+            try await res
+                .status(.ok)
+                .send(req.body)
+        }
+        .get("/greeting") { req, res in
+            let jsonBody = try await req.body.jsonObject()
+            let name = jsonBody["name"] as? String ?? ""
+            let result = "Hello, \(name)!"
+            let resultJson = [
+                "greeting": result
+            ]
+            let resultData = try JSONEncoder().encode(resultJson)
+            try await res
+                .status(.ok)
+                .send(resultData)
+        }
 
     public static func main() async throws {
-        try await onIncomingRequest(handler)
+        //try await onIncomingRequest(handler)
+        try await onIncomingRequest(router.run(_:_:))
     }
 
-    static func handler(req: IncomingRequest, res: OutgoingResponse) async throws {
-        let jsonBody = try await req.body.jsonObject()
-        let name = jsonBody["name"] as? String ?? ""
-        let result = "Hello, \(name)!"
-
+//    static func handler(req: IncomingRequest, res: OutgoingResponse) async throws {
+//        let jsonBody = try await req.body.jsonObject()
+//        let name = jsonBody["name"] as? String ?? ""
+//        let result = "Hello, \(name)!"
+//
 //        let resultJson = [
-//            "greeting": result,
+//            "greeting": result
 //        ]
 //        let resultData = try JSONEncoder().encode(resultJson)
-
-        try await res
-            .status(.ok)
-            .send(result)
-//            .send(resultData)
-    }
+//
+//        try await res
+//            .status(.ok)
+//            .send(result)
+////            .send(resultData)
+//    }
 }
